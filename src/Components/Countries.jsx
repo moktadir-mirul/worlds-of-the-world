6c4893f6488f9a335e8d3afe07b87beccebe4577
moktadir-mirul <moktadir.mirul@gmail.com>
2025-04-11 07:18:13 +0600

    country hown and handle visit done

--- src/Components/Countries.jsx
+++ src/Components/Countries.jsx
@@ -1,4 +1,5 @@
 import React, { use } from "react";
+import Country from "./Country/Country";
 
 
 function Countries({countriesPromise}) {
@@ -8,6 +9,11 @@ function Countries({countriesPromise}) {
     return (
         <div>
             <h1 className="in text-center text-5xl text-rose-900 py-5">Total Countries: {allCountries.length}</h1>
+            <div className="p-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+            {
+                allCountries.map(country => <Country key={country.cca3} country={country}></Country>)
+            }
+            </div>
         </div>
     )
 }
---
+++ src/Components/Country/Country.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+
+
+function Country ({country}) {
+    const [visited, setVisited] = useState(false);
+
+    const handleVisit = () => {
+        setVisited(!visited);
+    }
+
+    return (
+        <div>
+            <div className={`card bg-base-100 w-96 shadow-sm border border-gray-300 ${visited && 'bg-gray-300 border-2 border-rose-300'}`}>
+                <figure>
+                    <img
+                    className="p-2 w-full h-60 object-cover"
+                    src={country.flags.png}
+                    alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title text-2xl">{country.name.common}</h2>
+                    <div className="text-lg">
+                        <p>{country.name.official}</p>
+                        <p>Population: {country.population}</p>
+                        <p>Region: {country.region}</p>
+                        <p>Capital: {country.capital}</p>
+                    </div>
+                    <div className="card-actions justify-end">
+                    <button className={`btn ${visited ? 'btn-secondary' : 'btn-primary'}`} onClick={handleVisit}>
+                        {
+                            visited ? 'Visited' : 'Note Visited'
+                        }
+                    </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default Country;
